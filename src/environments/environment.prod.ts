e762001ce551a5578454f3d14d7a235e9ae6dbbc
Micah <Micah.dittmar@gmail.com>
2026-01-19 10:13:08 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Update this URL after deploying backend to Railway
-  apiUrl: 'https://YOUR-RAILWAY-APP.up.railway.app'
+  apiUrl: 'https://food-orb-production.up.railway.app'
 };
